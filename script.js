76b29a106166bf295cf240db2c019df40fe3ece5
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-20 09:38:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  var table = document.getElementById('sampleTable');
+	var row=document.createElement('tr');
+	var col1=document.createElement('td');
+	var col2=document.createElement('td');
+	col1.innerText='New Cell1';
+	col2.innerText='New Cell2';
+	row.appendChild(col1);
+	row.appendChild(col2);
+	table.prepend(row);
 }
